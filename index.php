092cc3477642bb212a70c2e356e481c3eb4526b7
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-09-05 14:57:55 -0300

    Excluir se fosse por e-mail

--- index.php
+++ index.php
@@ -2,37 +2,29 @@
 include 'contato.class.php';
 
 $contato = new Contato();
-$contato->adicionar('suporte@b7web.com','Suporte b7web');
-$contato->adicionar('fulano@b7web.com');
-
-
-$nome = $contato->getNome('suporte@b7web.com');
-
-echo "Nome: ".$nome."<br/>";
-
-$lista = $contato->getAll();
-
-print_r($lista);
-
-$contato->editar('Fulano', 'fulano@b7web.com');
-$nome = $contato->getNome('fulano@b7web.com');
-
-echo "<br/>Nome: ".$nome."<br/>";
-
-$excluir = $contato->excluir('ciclano@gmail.com');
-
-if($excluir == TRUE){
-    echo 'Foi Excluído!';
-} else {
-    echo 'E-mail para exclusão não existe!';
-}
-
-//$excluir = $contato->excluir('fulano@b7web.com');
-
-if($excluir == TRUE){
-    echo 'Foi Excluído!';
-} else {
-    echo 'E-mail para exclusão não existe!';
-}
-
 ?>
+<h1>Contatos</h1>
+<table border="1" width="600">
+	<tr>
+    	<th>ID</th>
+    	<th>NOME</th>
+    	<th>E-MAIL</th>
+    	<th>AÇÕES</th>
+	</tr>
+	
+	<?php 
+	$lista = $contato->getAll();
+	foreach($lista as $item):
+	?>
+	<tr>
+    	<td><?php echo $item['id'];?></td>
+    	<td><?php echo $item['nome'];?></td>
+    	<td><?php echo $item['email'];?></td>
+    	<td>
+    		<a href="">[ EDITAR ]</a>
+    		<a href="excluir.php?email=<?php echo $item['email']?>">[ EXCLUIR ]</a>
+    	</td>
+	</tr>
+	<?php endforeach;?>
+	
+</table>
