19c9f51bd676266774d8def6079d12bb02ad0862
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-09-05 11:40:17 -0300

    Create e Read

--- contato.class.php
+++ contato.class.php
@@ -5,7 +5,55 @@ class Contato{
     
     public function __construct(){
         $this->pdo = new PDO("mysql:dbname=crudoo;host=localhost", "admin", "admin@12");
-        echo "conectou";
+    }
+    
+    public function adicionar($email, $nome = ''){//create
+        // Verificar se já existe senão adiciona
+        if ($this->existeEmail($email) == FALSE){
+            $sql = "INSERT INTO contatos (nome, email) VALUES (:nome, :email)";
+            $sql = $this->pdo->prepare($sql);
+            $sql->bindValue(':nome', $nome);
+            $sql->bindValue(':email', $email);
+            $sql->execute();
+            return TRUE;
+            
+        }else {
+            return FALSE;
+        }
+        
+    }
+    
+    public function getNome($email){//Read
+        $sql = "SELECT nome FROM contatos WHERE email = :email";
+        $sql = $this->pdo->prepare($sql);
+        $sql->bindValue(':email', $email);
+        $sql->execute();
+        
+        if($sql->rowCount() > 0){
+            $info = $sql->fetch();
+            
+            return $info['nome'];
+        } else {
+            return '';
+        }
+        
+    }
+    
+    public function getAll(){//Read
+        $sql = "SELECT * FROM contatos";
+        $sql = $this->pdo->query($sql);
+        $sql->execute();
+        
+        if($sql->rowCount() > 0){
+            return $sql->fetchAll();
+        } else {
+            return array();
+        }
+        
+    }
+        
+    private function existeEmail($email){
+        
     }
 }
 ?>
