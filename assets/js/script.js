595bd481a4229cb90190eb63257f0cd2adc2b4ea
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-09-06 14:41:04 -0300

    Abrindo nosso modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 $(function() {
 	$('.modal_ajax').on('click', function(e) {
 		e.preventDefault();//Evitamos que o clique no botão abra a página que foi direcionada
+		$('.modal_bg').show(); //Abrindo nosso modal
 	});
 });
