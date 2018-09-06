a9b65adc96c290936852a03b601c94989aab22f8
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-09-06 14:43:59 -0300

    Mostrando o modal e insere o html carregando

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,6 @@ $(function() {
 	$('.modal_ajax').on('click', function(e) {
 		e.preventDefault();//Evitamos que o clique no botão abra a página que foi direcionada
 		$('.modal_bg').show(); //Abrindo nosso modal
+		$('.modal').html('Carregando ... '); //Mostrando o modal e insere o html carregando
 	});
 });
